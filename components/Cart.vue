b87aee8d01ba300502bc9be339fe440a210795ae
Hiep <huyhiep4520@gmail.com>
2022-03-16 20:25:50 +0700

    giaodien

---
+++ components/Cart.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="out">
+        <div class="outurl">
+            <v-container>
+                <div class="url">Trang chủ > Giỏ hàng</div>
+            </v-container>
+        </div>
+        <v-container>
+            <div class="header">
+                Giỏ sách <span class="headertext">(có 2 quyển sách trong giỏ hàng của bạn)</span>
+            </div>
+            <hr/>
+            <span class="note">Đảm bảo số lượng sách, mỗi độc giả mượn tối thiểu mỗi quyển 1 loại *</span>
+            <v-layout row wrap class="cartProduct x-grid-lg">
+                <v-flex lg2 sm12 xs12 class="pa-2">
+                    <div class="outImg">
+                        <img src="img/sach1.jfif" alt="" class="img">
+                    </div>
+                </v-flex>
+                <v-flex lg10 sm12 xs12 class="infoCart pa-2">
+                    <h3 class="name">Hướng dẫn đẹp trai lên một cách lạ thường</h3>
+                    <span class="author">Khá Bách</span>
+                    <span class="author">ID:S1319714</span>
+                    <span class="btnDelete">Xóa sách</span>
+                </v-flex>
+            </v-layout>
+            <div class="Register">
+                <span class="btnRegister" @click="changeRegister">Tạo phiếu mượn</span>
+            </div>
+        </v-container>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        checkRegister:Boolean
+    },
+    data(){
+        return {
+            check:this.checkRegister
+        }
+    },
+    methods: {
+        changeRegister(){
+            this.check=true
+            this.$emit("update-check", this.check);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.outurl{
+    width: 100%;
+    border-bottom: 1px solid #999;
+}
+.url{
+    padding: 0;
+    font-size: 15px;
+    color: #999;
+}
+.header{
+    padding: 10px 0;
+    font-size: 20px;
+    font-weight: 500;
+    color: #333;
+    display: flex;
+    align-items: center;
+}
+.headertext{
+    font-size:14px;
+    color: #777;
+    font-weight: 400;
+    margin-left: 5px;
+}
+.note{
+    margin-top: 10px;
+    color: red;
+    font-size:14px;
+    display:block;
+}
+.cartProduct{
+    margin-top: 20px;
+    padding: 10px 0;
+    border-bottom: 1px solid rgb(216, 214, 214);
+}
+.outImg{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.img{
+    width: 100%;
+    height:180px;
+}
+.name{
+    font-weight: 500;
+    color:#333;
+}
+.author{
+    display: block;
+    color:#777;
+    font-size: 16px;
+    margin-top:5px;
+}
+.btnDelete{
+    width:150px;
+    padding:10px 0;
+    text-align: center;
+    margin-top:10px;
+    box-shadow: 0 0 3px #999;
+    color:red;
+    font-weight: 500;
+    border-radius: 3px;
+    cursor: pointer;
+}
+.btnDelete:hover{
+    background-color:rgb(216, 14, 14);
+    color:white;
+}
+.infoCart{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+}
+.Register{
+    display: flex;
+    margin: 20px 0;
+    justify-content:right;
+}
+.btnRegister{
+    padding:14px 25px;   
+    text-align: center;
+    box-shadow: 0 0 3px #999;
+    color:white;
+    background-color:green;
+    font-weight: 500;
+    border-radius: 3px;
+    cursor: pointer;
+}
+.btnRegister:hover{
+    opacity: 0.9;
+}
+</style>
---
+++ components/Deltail.vue
@@ -0,0 +1,98 @@
+<template>
+    <v-container>
+        <div class="url">Trang chủ > Deltail</div>
+        <v-layout row wrap class="x-grid-lg">
+            <v-flex lg6 sm12 xs12 class="pa-2">
+                <div class="outImg">
+                    <img src="img/sach1.jfif" alt="" class="Img">
+                </div>
+            </v-flex>
+            <v-flex lg6 sm12 xs12 class="info pa-2">
+                <h2 class="name">Tên sách</h2>
+                <v-layout row wrap class="x-grid-lg">
+                    <v-flex lg4 sm12 xs12 class="pa-2">
+                        <span class="author">Tác phẩm</span>
+                        <span class="author">Nhà xuất bản</span>
+                        <span class="author">Năm xuất bản</span>
+                        <span class="author">ID:S9164146</span>
+                    </v-flex>
+                    <v-flex lg8 sm12 xs12 class="pa-2">
+                        <span class="content">Phạm Huy Hiệp</span>
+                        <span class="content">Anh Hịp</span>
+                        <span class="content">2000</span>
+                    </v-flex>
+                </v-layout>
+                <div class="btnBorrow">
+                    <span class="btn">Mượn</span>
+                </div>
+                <hr/>
+                <v-layout row wrap class="x-grid-lg">
+                    <v-flex lg4 sm12 xs12 class="pa-2">
+                        <span class="author">Mã hàng</span>
+                        <span class="author">Thể loại</span>
+                    </v-flex>
+                    <v-flex lg8 sm12 xs12 class="pa-2">
+                        <span class="content">N/A</span>
+                        <span class="content">Văn học</span>
+                    </v-flex>
+                </v-layout>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.url{
+    margin-bottom:20px;
+    font-size:16px;
+    color:#888;
+}
+.outImg {
+    width: 100%;
+    height: 600px;
+    display: flex;
+    align-items: center;
+    justify-content:center;
+}
+.Img{
+    width:100%;
+    height: 100%;
+}
+.info{
+    padding: 20px;
+}
+.name{
+    font-weight: 400;
+    text-transform: capitalize;
+}
+.author{
+    color: #999;
+    font-size: 15px;
+    display:block;
+    margin:15px 0;
+}
+.content{
+    color: #333;
+    font-size: 15px;
+    display:block;
+    margin:15px 0;
+}
+.btnBorrow{
+    margin:20px 10px 50px 10px ;
+}
+.btn{
+    padding:15px 80px;
+    background-color: #f5f5f5;
+    color: red;
+    box-shadow: 0 2px 5px #999;
+    font-weight: 600;
+    border-radius:3px;
+}
+.btn:hover{
+    background-color: rgb(230, 230, 230);
+}
+</style>
